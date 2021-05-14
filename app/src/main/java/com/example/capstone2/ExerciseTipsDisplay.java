00102facea8e28d0f7c0cb5378be797d617c574b
heebinYoo <r.inc.md@gmail.com>
2021-05-14 15:53:28 +0900

    팁 유투브에 제대로 올려서 정리 완료
    TODO
    칼로리 측정 모델을 새로 만든 모델로 대체하고, 운동별로 기능 바뀔 수 있게 수정하는 작업

--- app/src/main/java/com/example/capstone2/ExerciseTipsDisplay.java
+++ app/src/main/java/com/example/capstone2/ExerciseTipsDisplay.java
@@ -12,9 +12,9 @@ import android.widget.LinearLayout;
 
 public class ExerciseTipsDisplay extends AppCompatActivity {
 
+    private Button btn_a;
     private Button btn_s;
-    private Button btn_d;
-    private Button btn_p;
+    private Button btn_l;
     private View.OnClickListener t_listener;
     private LinearLayout container;
 
@@ -24,34 +24,38 @@ public class ExerciseTipsDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_tips_display);
-       // btn_d = (Button)findViewById(R.id.deadlift);
+        btn_a = (Button)findViewById(R.id.arm);
         btn_s = (Button)findViewById(R.id.squat);
-        //btn_p = (Button)findViewById(R.id.plank);
+        btn_l = (Button)findViewById(R.id.leg);
 
         container = (LinearLayout)findViewById(R.id.container);
 
-//        btn_d.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//                inflater.inflate(R.layout.youtube_d, container, true);
-//            }
-//        });
+        btn_a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                container.removeAllViews();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.youtube_a, container, true);
+
+            }
+        });
 
         btn_s.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                container.removeAllViews();
                 LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                inflater.inflate(R.layout.youtube_d, container, true);
+                inflater.inflate(R.layout.youtube_s, container, true);
             }
         });
 
-//        btn_p.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//                inflater.inflate(R.layout.youtube_d, container, true);
-//            }
-//        });
+        btn_l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                container.removeAllViews();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.youtube_l, container, true);
+            }
+        });
     }
 }
